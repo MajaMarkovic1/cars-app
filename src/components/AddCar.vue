c755cc2a6ab396bf820a0f26fe3c255e77fd1015
Maja Markovic <markovicm9000@gmail.com>
2018-08-15 13:22:29 +0200

    zadatak6

--- src/components/AddCar.vue
+++ src/components/AddCar.vue
@@ -5,7 +5,8 @@
             <label for="brand" class="col-4 col-form-label">Brand</label>
             <div class="col-8">
               <div class="input-group">
-                <input id="brand" name="brand" type="text" required="required" class="form-control here" v-model="car.brand">
+                <input id="brand" name="brand" type="text" required="required" class="form-control here" 
+                    minlength="2" v-model="car.brand">
               </div>
             </div>
           </div>
@@ -13,15 +14,18 @@
             <label for="model" class="col-4 col-form-label">Model</label>
             <div class="col-8">
               <div class="input-group">
-                <input id="model" name="model" type="text" required="required" class="form-control here" v-model="car.model">
+                <input id="model" name="model" type="text" required="required" class="form-control here" 
+                    minlength="2" v-model="car.model">
               </div>
             </div>
           </div>
           <div class="form-group row">
             <label for="year" class="col-4 col-form-label">Year</label>
             <div class="col-8">
-                <select class="input-group" v-model="car.year" >
-                    <option v-for="year in years" :key="year" :value="year" class="form-control here">{{ year }}</option>
+                <select class="input-group" v-model="car.year" required="required">
+                    <option v-for="year in years" :key="year" :value="year" class="form-control here">
+                        {{ year }}
+                    </option>
                 </select>
             </div>
           </div>
@@ -29,7 +33,7 @@
             <label for="maxSpeed" class="col-4 col-form-label">Max speed</label>
             <div class="col-8">
               <div class="input-group">
-                <input id="maxSpeed" name="maxSpeed" type="number" required="required" class="form-control here" v-model="car.maxSpeed">
+                <input id="maxSpeed" name="maxSpeed" type="number" class="form-control here" v-model="car.maxSpeed">
               </div>
             </div>
           </div>
@@ -46,16 +50,24 @@
               <div class="col-8"> 
                 <div class="radio">
                     <div class="radio">
-                        <label><input type="radio" name="optradio" v-model="car.engine" value="diesel">diesel</label>
+                        <label><input type="radio" name="optradio" required="required" v-model="car.engine" value="diesel">
+                            diesel
+                        </label>
                     </div>
                     <div class="radio">
-                        <label><input type="radio" name="optradio" v-model="car.engine" value="petrol">petrol</label>
+                        <label><input type="radio" name="optradio" required="required" v-model="car.engine" value="petrol">
+                            petrol
+                        </label>
                     </div>
                     <div class="radio disabled">
-                        <label><input type="radio" name="optradio" v-model="car.engine" value="electric">electric</label>
+                        <label><input type="radio" name="optradio" required="required" v-model="car.engine" value="electric">
+                            electric
+                        </label>
                     </div>
                     <div class="radio disabled">
-                        <label><input type="radio" name="optradio" v-model="car.engine" value="hybrid">hybrid</label>
+                        <label><input type="radio" name="optradio" required="required" v-model="car.engine" value="hybrid">
+                            hybrid
+                        </label>
                     </div>
                 </div>
                 </div>
